1f67d9be13f446859a3d8e412b75c12d9027e281
west <west@west-dynamics-1.0>
2022-11-28 13:55:37 +0200

    Start of Index Page

--- php/src/project/login.php
+++ php/src/project/login.php
@@ -1,3 +1,7 @@
+<!-- 
+This is the login page determines which call to make when session loads
+-->
+
 <?php
 
 $host="db";
@@ -5,17 +9,18 @@ $user="MYSQL_USER";
 $password="MYSQL_PASSWORD";
 $db="MYSQL_DATABASE";
 session_start();
-
+// Checks database connection
 $data=mysqli_connect($host, $user, $password, $db);
 if($data ===false){
     die("connection failed");
 }
-
+//Listens for the submit button
+//takes values and matches them to the databe and logs them in by usertype
 if($_SERVER["REQUEST_METHOD"]=="POST"){
     $username = $_POST["username"];
     $password = $_POST["password"];
 
-    $sql="select * from login where username='".$username."' AND password='".$password."' ";
+    $sql="select * from users where username='".$username."' AND password='".$password."' ";
     $result=mysqli_query($data,$sql);
     $row=mysqli_fetch_array($result);
     if($row["usertype"]=="user"){
