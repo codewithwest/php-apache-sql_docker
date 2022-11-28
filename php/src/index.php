6c00100ecd88ed891c75cad0f900ebeca41156e4
Jonas Lekgau <77699400+codewithwest@users.noreply.github.com>
2022-11-28 07:46:44 +0200

    commit

---
+++ php/src/index.php
@@ -0,0 +1,64 @@
+<?php
+
+$host="db";
+$user="MYSQL_USER";
+$password="MYSQL_PASSWORD";
+$db="MYSQL_DATABASE";
+session_start();
+
+$data=mysqli_connect($host, $user, $password, $db);
+if($data ===false){
+    die("connection failed");
+}
+
+if($_SERVER["REQUEST_METHOD"]=="POST"){
+    $username = $_POST["username"];
+    $password = $_POST["password"];
+
+    $sql="select * from users where username='".$username."' AND password='".$password."' ";
+    $result=mysqli_query($data,$sql);
+    $row=mysqli_fetch_array($result);
+    if($row["usertype"]=="user"){
+        $_SESSION["username"]=$username;
+        header("location:userhome.php");
+    }
+    elseif($row["usertype"]=="admin"){
+        $_SESSION["username"]=$username;
+        header("location:adminhome.php");
+    }
+    else {
+        echo "username or password incorrect";
+    }
+}
+
+
+?>
+
+
+ <!DOCTYPE html>
+ <html lang="en">
+ <head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Document</title>
+ </head>
+ <body>
+ <center>
+    <form action="#" method="POST">
+    <div >
+        <label for="">username</label>
+        <input type="text" name="username" required>
+    </div>
+    <div>
+        <label for="">password</label>
+        <input type="password" name="password" required>
+    </div>
+    <div>
+     
+        <input type="submit" value="Login" >
+    </div>
+    </form>
+   </center>
+ </body>
+ </html>
